92b84d2e48cbe5623655b9afd6e61905d31e90be
Roman Bunyak <compcc@ya.ru>
2023-01-11 21:17:27 +0300

    add Example001 and Example002

--- Example003_Sum/Program.cs
+++
@@ -1,6 +0,0 @@
-int numberA = new Random().Next(1,10); //1, 2, 3 ..... 9
-int numberB = new Random().Next(1,10);
-int result = numberA + numberB;
-Console.WriteLine(numberA);
-Console.WriteLine(numberB);
-Console.WriteLine(result);
