793cdd353240366c7b0283b68b833a0755b3dfcc
Roman Bunyak <compcc@ya.ru>
2023-01-11 21:51:16 +0300

    add Eample005

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "света")
+{
+    Console.WriteLine("Привет любимая!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.Write(username);
+}
+
