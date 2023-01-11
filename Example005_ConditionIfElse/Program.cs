5b97816e62bb211465f66418265c7a0e2a05f434
Roman Bunyak <compcc@ya.ru>
2023-01-11 19:55:30 +0300

    fix bug

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-﻿Console.Write("Введите имя пользователя:");
+Console.Write("Введите имя пользователя:");
 string username = Console.ReadLine();
-if(username.ToLower() == "Света");
+if(username.ToLower() == "Света")
 {
     Console.WriteLine("Приветики, Светик!");
 }
 else
 {
     Console.Write("Привет, ");
-=======
-Console.WriteLine("Введите ваше имя:");
-string username = Console.ReadLine();
-if(username == "Маша");
-{
-    Console.WriteLine("Ура Маша");
 }
-else 
-{
-    Console.WriteLine("Привет,");
->>>>>>> main
-    Console.WriteLine(username);
-}
-
