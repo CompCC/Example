cb86ecc0bd2754a49d58fda9c9695fb5b4c13754
Roman Bunyak <compcc@ya.ru>
2023-01-15 14:30:48 +0300

    add Lesson 15.01.23

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 22, 31, 22, 24, 55, 61, 77, 38 };
+
+int n = array.Length;
+// нахождение индекса в массиве
+int find = 22;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
